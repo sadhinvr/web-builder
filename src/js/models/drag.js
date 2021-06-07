afb32077ffa45deed4b1a7023862ea3d862b4f3b
sadhin <sadhinvr@gmail.com>
2021-06-07 17:25:25 +0600

    fix drag

--- src/js/models/drag.js
+++ src/js/models/drag.js
@@ -19,13 +19,16 @@ import {
     on,
     setStyleData
 } from '../views/iframeView';
-import { getStyle } from './style';
-import { navOn } from './navigator';
+import {
+    getStyle
+} from './style';
+import {
+    navOn
+} from './navigator';
 
 const dragable = $('.dragable', true);
 let data, dragging = false,
-    ifr = false,
-    appended, p, pos, active;
+    appended, p, pos, active, dragState;
 
 const activeBox = idocument.createElement('div');
 activeBox.id = 'activeBox';
@@ -34,63 +37,50 @@ dragable.forEach(cur => {
     cur.addEventListener('mousedown', mousedown)
 })
 
-const dumb=document.createElement('div');
 
 function mousedown(e) {
     e.preventDefault();
 
     if (e.currentTarget.dataset.drag && e.button === 0) {
-        
 
         //set sate 
-        if (e.currentTarget.dataset.drag === '10') {
-            ifr = true;
-        }
-
         appended = true;
         dragging = true;
-
+        dragState = e.currentTarget.dataset.drag;
         // set data
         setDragData(e);
-
-        // ready clone
-        readyDragClone();
-
-        // scroll position
         data.winY = iwindow.scrollY;
-        data.winX=iwindow.scrollX;
+        data.winX = iwindow.scrollX;
 
-        // active
-        if (!ifr) {
-            if (active) {
-                // console.log(active)
-                // pos = mouseOver(e, active.dataset.ele,true);
-            }
-        } else {
-            data.node.style.opacity='.5';
+
+
+
+        //check drag 10/11/20
+        if (dragState === '10') {
+            data.node.style.opacity = '.5';
             //active
             active = data.node;
             idocument.getElementById('dev').appendChild(activeBox);
             setStyleData(active, activeBox, '1px solid #00ff13')
-
             //active style
             getStyle(active);
-            
-        }
+        } else if (dragState === '11') {
 
-        // change cursor
-        cCursor();
+        } else if (dragState === '12') {
+
+        }
 
         // mouse over
         window.addEventListener('mouseover', appendPos);
         iwindow.addEventListener('mouseover', iappendPos);
 
         //close menus
-        const right_menu=$('.left .right_menu');
-        right_menu.style.display='none';
-        [].forEach.call(right_menu.children,cur=>{
-            cur.style.display='none';
+        const right_menu = $('.left .right_menu');
+        right_menu.style.display = 'none';
+        [].forEach.call(right_menu.children, cur => {
+            cur.style.display = 'none';
         })
+
     }
 
     if (e.target.dataset.ele && e.target.dataset.ele === 'body') {
@@ -102,10 +92,9 @@ function mousedown(e) {
         getStyle();
 
     }
-
-
-
 }
+
+
 
 function cCursor() {
     if (dragging) {
@@ -129,11 +118,13 @@ function iappendPos(e) {
     window.removeEventListener('mousemove', drag);
     p = idocument.body.querySelector('#dev');
     iwindow.addEventListener('mousemove', drag);
-
-
 }
 
 function drag(e) {
+    e.preventDefault()
+    // change cursor
+    cCursor();
+
     if (on || navOn) {
         document.body.classList.remove('not_allowed');
     } else {
@@ -143,8 +134,7 @@ function drag(e) {
     //position 
     pos = mouseOver(e, data.node);
     // append child
-
-    if (!ifr) {
+    if (dragState === '11') {
         p.appendChild(data.clone);
         data.clone.style.top = `${e.clientY}px`;
         data.clone.style.left = `${e.clientX}px`;
@@ -161,76 +151,74 @@ function scrollWin(e) {
     const ifrWidth = iwindow.innerWidth;
 
     //bottom
-    if (ifrHeight-50 < e.clientY) {
+    if (ifrHeight - 50 < e.clientY) {
         data.winY += speed;
     }
-    
+
     //top
     else if (50 > e.clientY) {
         data.winY -= speed;
     }
 
     //right
-    else if (ifrWidth-50 < e.clientX) {
+    else if (ifrWidth - 50 < e.clientX) {
         data.winX += speed;
     }
 
     //left
     else if (50 > e.clientX) {
         data.winX -= speed;
-    }
-    else{
-        data.winY=iwindow.scrollY;
-        data.winX=iwindow.scrollX;
+    } else {
+        data.winY = iwindow.scrollY;
+        data.winX = iwindow.scrollX;
     }
 
-    iwindow.scrollTo(data.winX,data.winY);
+    iwindow.scrollTo(data.winX, data.winY);
 }
 
 function setDragData(e) {
     data = {
         winY: iwindow.scrollY,
-        winX:iwindow.scrollX,
+        winX: iwindow.scrollX,
         node: e.target,
         top: e.currentTarget.getClientRects()[0].top,
         left: e.currentTarget.getClientRects()[0].left,
     };
 
-    if (!ifr) {
+    if (dragState === '11') {
         data.node = e.currentTarget,
             data.clone = e.currentTarget.cloneNode(true);
         data.height = e.currentTarget.getClientRects()[0].height;
         data.width = e.currentTarget.getClientRects()[0].width;
+        readyDragClone();
     }
-    
-
 }
 
 function readyDragClone() {
-    if (!ifr) {
-        data.clone.style.width = `${data.width}px`;
-        data.clone.style.height = `${data.height}px`;
 
-        data.clone.style.top = `${data.top}px`;
-        data.clone.style.left = `${data.left}px`;
-        data.clone.style.position = `fixed`;
-        data.clone.classList.add('fade');
-    }
+    data.clone.style.width = `${data.width}px`;
+    data.clone.style.height = `${data.height}px`;
+
+    data.clone.style.top = `${data.top}px`;
+    data.clone.style.left = `${data.left}px`;
+    data.clone.style.position = `fixed`;
+    data.clone.classList.add('fade');
+
 }
 
 function resetDrag() {
-        document.body.className.includes('not_allowed')?document.body.classList.remove('not_allowed'):0;
-        idocument.body.className.includes('not_allowed')?idocument.body.classList.remove('not_allowed'):0;
+    document.body.className.includes('not_allowed') ? document.body.classList.remove('not_allowed') : 0;
+    idocument.body.className.includes('not_allowed') ? idocument.body.classList.remove('not_allowed') : 0;
 
-    if (!ifr) {
+    if (dragState === '11') {
         // window.removeEventListener('mousemove', drag);
 
         // remove fade
         data.clone.remove('fade');
 
 
-    } else {
-        data.node.style.opacity='';
+    } else if (dragState === '10') {
+        data.node.style.opacity = '';
         idocument.body.classList.remove('cursor_dragging');
         // iwindow.removeEventListener('mousemove', drag);
 
@@ -253,15 +241,15 @@ function resetDrag() {
 function mouseup() {
     if (dragging) {
         resetDrag();
-        if (!ifr) {
+        if (dragState === '11') {
             drop(data, pos);
         } else {
             drop(data, pos, false);
         }
     }
     dragging = false;
-    ifr = false;
-    pos=[];
+    dragState = undefined;
+    pos = [];
     // change cursor
     cCursor();
 }
