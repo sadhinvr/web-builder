31bbfb5ab000285ee1eee3ccdf01c2f08dcd69f9
sadhin <sadhinvr@gmail.com>
2021-09-05 18:30:30 +0600

    fix html - iframe

--- dist/js/reuse.js
+++ dist/js/reuse.js
@@ -3,7 +3,9 @@ const iframe = document.getElementById('iframe');
 const domEle = {
     ibody:iframe.contentDocument,
     body:document.body,
-    iresize:$('.iresize')
+    iresize:$('.iresize',true),
+    i:$('.i'),
+    i_holder:$('.i-holder'),
 }
 
 function $(q, a = false ,i = false) {
@@ -20,3 +22,29 @@ function $(q, a = false ,i = false) {
     }
     return document.querySelector(q);
 }
+
+
+// function setStyleData(e, box, bor = false, bg = false) {
+//     box.setAttribute('focusable', "false");
+//     box.style = 'position:absolute;pointer-events: none;';
+//     let style = e.currentStyle || window.getComputedStyle(e),
+//         width = e.offsetWidth,
+//         height = e.offsetHeight,
+//         margin1 = parseFloat(style.marginLeft) + parseFloat(style.marginRight),
+//         margin2 = parseFloat(style.marginTop) + parseFloat(style.marginBottom),
+//         padding = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight),
+//         border = parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
+
+//     // change style
+//     box.style.top = e.offsetTop - parseFloat(style.marginTop) + 'px';
+//     box.style.left = e.offsetLeft - parseFloat(style.marginLeft) + 'px';
+
+//     box.style.width = width + margin1 + 'px';
+//     box.style.height = height + margin2 + 'px';
+//     if (e.dataset.ele && e.dataset.ele === 'body') {
+//         box.style.top = e.offsetTop + 'px';
+//         box.style.left = e.offsetLeft + 'px';
+//     }
+
+//     bor ? box.style.border = bor : box.style.background = bg;
+// }
--- src/js/controller/drag.js
+++ src/js/controller/drag.js
@@ -3,7 +3,7 @@ import Action from "../models/Action"
 const dragAction = new Action(domEle.body);
 dragAction.on('mousedown',e=>{
     console.log('mousedown');
-    const arr = [domEle.iresize];
+    const arr = [...domEle.iresize];
     if(arr.includes(e.target)){
         e.preventDefault();
         e.stopPropagation();
@@ -15,9 +15,20 @@ dragAction.on('mousedown',e=>{
 function mousemove(e){
     console.log('moved');
     const pos = dragAction.mousePos(e);
+    const rect = domEle.i.getBoundingClientRect().width;
+    const rect2 = domEle.i_holder.getBoundingClientRect().width;
+
+    let iwidth  = rect.width+pos.rX;
+    if(iwidth){
+        
+    }
+
+    domEle.i.style.width=rect.width+pos.rX;
+    let scale = domEle.i.getBoundingClientRect().width;
+    if(scale)
+    iframe.style.transform= `scale(${i})`
+
     
-
-
 }
 
 dragAction.on('mouseup',e=>{
