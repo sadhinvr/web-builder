8ca622b63af70fd9c11d9758b96acc495b90a696
sadhin <sadhinvr@gmail.com>
2021-09-17 00:20:30 +0600

    drag & drop

--- src/js/controller/drag.js
+++ src/js/controller/drag.js
@@ -5,10 +5,8 @@ import {
 } from '../models/sizing';
 import { htmlMockup } from "../views/mockup";
 
-
 const dragAction = new Action(domEle.body);
 const mm = e => iResize(e, dragAction);
-
 
 const rmm = e => {
     iframe.style.pointerEvents = '';
@@ -17,8 +15,9 @@ const rmm = e => {
 }
 
 dragAction.pX = 0;
+
 dragAction.on('mousedown', e => {
-    console.log('mousedown');
+    // console.log('mousedown');
     dragAction.removeEvent('mouseup', rmm);
 
     if ([...domEle.iresize].includes(e.target)) {
@@ -111,5 +110,96 @@ dragAction.on('mousedown', e => {
         $('.js-elebar').click();
     }
 
-
 });
+
+dragAction.on('mousedown',e=>{
+    if (e.target.dataset.sb_ele) {
+        
+
+        dragAction.eventSettings(e, {
+            pd: true,
+            stpro: true,
+            point: false
+        }, []);
+
+        const child = e.target;
+
+
+        child.style.opacity = '0.5';
+        // const rect = e.target.getBoundingClientRect();
+        // const irect = iframe.getBoundingClientRect();
+        // const div = document.createElement('div');
+        let parent;
+        let pos;
+        // div.classList.add('js-eleHolder');
+        // div.style.position="fixed";
+        // div.innerHTML = e.target.outerHTML;
+        // div.style.zIndex= '9999999';
+        // div.style.pointerEvents = 'none';
+        // div.style.width = rect.width+'px';
+        // div.style.height = rect.height+'px';
+        // div.style.display = 'flex';
+        const dp = e => {
+            // $('js-eleHolder') || document.body.appendChild(div);
+            // div.style.top= `${e.clientY}px`;
+            // div.style.left= `${e.clientX}px`;
+            // console.log(e.clientX);
+        }
+        const dpi = e => {
+            // $('js-eleHolder') || document.body.appendChild(div);
+            // div.style.top= `${irect.top+e.clientY}px`;
+            // div.style.left= `${irect.left+e.clientX}px`;
+             pos= mouseOver(e,child);
+            // console.log(e.clientX);
+            
+        }
+
+        const mo = e=>{
+            parent = e;
+            document.body.style.cursor = 'grabbing';
+            domEle.idoc.body.style.cursor = 'grabbing';
+            // console.log(e.target)
+        };
+        dragAction.on('mousemove',dp);
+        dragAction.on('mousemove',dpi,domEle.idoc);
+        dragAction.on('mouseover',mo);
+        dragAction.on('mouseover',mo,domEle.idoc);
+
+        dragAction.on('mouseup', e => {
+            dragAction.removeEvent('mousemove', dpi,domEle.idoc);
+            dragAction.removeEvent('mouseover',mo,domEle.idoc);
+            dragAction.removeEvent('mousemove', dp);
+            dragAction.removeEvent('mouseover',mo);
+            // div.remove();
+            $('[data-sb="curpos"]',false,true)?.remove();
+
+            // if(pos[1] && pos[0]){
+            //     const ele = htmlMockup[child.dataset.sb_ele].outerHTML;
+            //     pos[0].insertAdjacentHTML(pos[1],ele);
+            // }
+            pos = false;
+            document.body.style.cursor = '';
+            domEle.idoc.body.style.cursor = '';
+            child.style.opacity = '';
+        }).on('mouseup', e => {
+            dragAction.removeEvent('mousemove', dpi,domEle.idoc);
+            dragAction.removeEvent('mouseover',mo,domEle.idoc);
+            dragAction.removeEvent('mousemove', dp);
+            dragAction.removeEvent('mouseover',mo);
+            // div.remove();
+            $('[data-sb="curpos"]',false,true)?.remove();
+            if(pos[1] && pos[0]){
+                // const ele = htmlMockup[child.dataset.sb_ele].outerHTML;
+                pos[0].insertAdjacentElement(pos[1],child);
+            }
+
+            document.body.style.cursor = '';
+            domEle.idoc.body.style.cursor = '';
+            child.style.opacity = '';
+            pos = false;
+        },domEle.idoc)
+
+
+        // $('.js-elebar').click();
+    }
+},domEle.idoc.body);
